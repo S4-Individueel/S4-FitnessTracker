ba6468af9a97a35eac92ddd17879d15357a350a0
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-14 11:58:25 +0100

    Add core motion pedometer thjat counts 7 days in past

--- S4-FitnessTracker/ContentView.swift
+++ S4-FitnessTracker/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import CoreMotion
+
 
 struct ContentView: View {
+    private let pedometer: CMPedometer = CMPedometer()
+    
+    @State private var steps: Int?
+    
+    private var isPedometerAvailible: Bool {
+        return CMPedometer.isPedometerEventTrackingAvailable() && CMPedometer.isStepCountingAvailable() && CMPedometer.isDistanceAvailable()
+    }
+    
+    private func initializePedometer() {
+        if isPedometerAvailible {
+            guard let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date()) else { return }
+            
+            pedometer.queryPedometerData(from: startDate, to: Date()) { data, error in guard let data = data, error == nil else {return}
+                steps = data.numberOfSteps.intValue
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(steps != nil ? "Steps set last 7 days: \(steps!)" : "Steps set last 7 days: 0").padding()
+            
+            .onAppear {
+                initializePedometer()
+            }
         }
-        .padding()
     }
 }
 

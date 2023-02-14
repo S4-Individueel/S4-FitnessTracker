a94f3a216601d77dc60a4ed2d58b0ef75b354709
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-14 08:46:36 +0100

    First commit

---
+++ S4-FitnessTracker/S4_FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  S4_FitnessTrackerApp.swift
+//  S4-FitnessTracker
+//
+//  Created by Reno Muijsenberg on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct S4_FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
